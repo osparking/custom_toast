023d1aa201fc472dc9af13b3ba81afad54fb06c2
BumTiger <jbpark03@gmail.com>
2026-02-09 09:43:20 +0900

    맞춤 그릇 사용 모양새 지정

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import toast, { Toaster } from 'react-hot-toast'
+import toast, { Toaster } from "react-hot-toast";
+import "./App.css";
+import "./Toast.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Toaster
-        position="top-center"  // Change position here
+        position="top-center"
+        containerClassName="custom-toast-container"
         toastOptions={{
-          duration: 4000,
-          style: {
-            background: '#363636',
-            color: '#fff',
-          },
+          className: "custom-toast",
         }}
-      />    
-      <button 
+      />
+      <button
+        variant="success"
         onClick={() => {
-          toast.success('이것은 토스트 알림입니다!')
+          toast.success("상단 중간 아래로 200px 토스트예요.");
         }}
+        style={{backgroundColor: "pink", fontWeight: "bold", fontSize: "20px"}}
       >
         토스트 보기
       </button>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
