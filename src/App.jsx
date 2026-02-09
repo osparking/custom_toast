c0ce5be0a369d499f6b1912476c08f2fd34ee307
BumTiger <jbpark03@gmail.com>
2026-02-09 10:40:48 +0900

    동작하는 맞춤형 토스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import toast, { Toaster } from "react-hot-toast";
 import "./App.css";
 import "./Toast.css";
+import ToastTester from "./ToastTester";
 
 function App() {
-
   return (
     <>
-      <Toaster
-        position="top-center"
-        containerClassName="custom-toast-container"
-        toastOptions={{
-          className: "custom-toast",
-        }}
-      />
-      <button
-        variant="success"
-        onClick={() => {
-          toast.success("상단 중간 아래로 80px 토스트예요.");
-        }}
-        style={{backgroundColor: "pink", fontWeight: "bold", fontSize: "20px"}}
-      >
-        토스트 보기
-      </button>
+      <ToastTester />
     </>
   );
 }
